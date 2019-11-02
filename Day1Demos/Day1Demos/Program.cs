c9a0a7a421239f085631426b5749959a6896f0b6
Anthony Price <antprice07@gmail.com>
2019-11-02 13:59:02 -0400

    init commit

---
+++ Day1Demos/Day1Demos/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Day1Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello World!");
+            Console.Write("abc");
+            Console.WriteLine("xyz");
+
+            Console.WriteLine("Enter a name: ");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("name = " + str1);*/
+
+            /*Console.Write("Enter a whole number: ");
+            int i1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a whole number: ");
+            int i2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("sum = " + (i1 + i2));*/
+
+          /*  string choice = "y";
+            while (choice.Equals("y"))
+            {
+                Console.WriteLine("hey");
+                Console.Write("Continue? y/n: ");
+                choice = Console.ReadLine();
+            }*/
+
+         /*   if (i2 > 5)
+            {
+                Console.WriteLine(i2 + " is greater than 5.");
+            }
+            else
+            {
+                Console.WriteLine(i2 + " is less than 5.");
+            }*/
+
+            Random rand = new Random();
+            int r1 = rand.Next();
+            Console.WriteLine("Random number = " +r1);
+
+            Console.WriteLine("Generate random numbers between 1 and 10.");
+            for (int i = 0; i < 10; i++)
+            {
+                r1 = rand.Next(10);
+                Console.WriteLine("Random number = " + r1);
+            }
+
+            double d1 = 12345.67890;
+            Console.WriteLine("double = " + d1);
+            d1 = Math.Round(d1,2);
+            Console.WriteLine("double now = " + d1);
+
+            double price = d1;
+            Console.WriteLine("formatted price: " + price.ToString("C2"));
+
+            double pct = .036789;
+            Console.WriteLine("formatted pct: " + pct.ToString("P3"));
+            int i3 = 5;
+            switch (i3) {
+                case 1:
+                    Console.WriteLine("i3 = 1");
+                    break;
+                case 2:
+                    Console.WriteLine("i3 = 2");
+                    break;
+                case 3:
+                    Console.WriteLine("i3 = 3");
+                    break;
+                case 4:
+                    Console.WriteLine("i3 = 4");
+                    break;
+                case 5:
+                    Console.WriteLine("i3 = 5");
+                    break;
+                default:
+                    Console.WriteLine("Not between 1 and 5.");
+                    break;
+                }
+
+            Console.WriteLine("bye");
+
+        }
+    }
+}
---
+++ ProductManager/ProductManager/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ProductManager {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Product Manager");
+
+            int i1 = GetInt("Enter a whole number: ");
+            Console.WriteLine("i1 = " + i1);
+
+            String name = "";
+            name = GetString("Enter name: ");
+            Console.WriteLine("name is: " + name);
+
+
+            PrintThreeNumbers(5, 6, 7);
+            PrintThreeNumbers();
+            PrintThreeNumbers(b: 8,c: 10);
+
+            Product p1 = new Product("java", "Murach's Java Programming", 59.50);
+            Console.WriteLine(p1);
+            Book b1 = new Book { Code = "c#", Description = "Murach's C# Programming", Price = 57.50, Author = "Joel Murach" };
+            Console.WriteLine(b1);
+            Product p2 = new Product("and", "Murach's Android Programming", 62.50);
+            Product[] products = { p1, p2, b1 };
+
+            foreach(Product p in products) {
+                Console.WriteLine(p);
+                }
+            ArrayList productList1 = new ArrayList();
+            productList1.Add(p1);
+            productList1.Add(p2);
+            productList1.Add(b1);
+
+            Product p = (Product)productList1[0];
+            Console.WriteLine("Use a list...");
+
+            List<Product> productList2 = new List<Product>(0);
+            productList2.Add(p1);
+            productList2.Add(p2);
+            productList2.Add(b1);
+
+            Product p3 = productList2[0];
+            Dictionary<string,Product> productDict = new Dictionary<string, Product>();
+            productDict.Add(p1.Code,p1);
+            productDict.Add(p2.Code,p2);
+            productDict.Add(b1.Code,b1);
+
+            p = productDict["andr"];
+            Console.WriteLine(p);
+
+
+            Console.WriteLine("Bye.");
+
+            }
+
+        private static void PrintThreeNumbers(int a = 1, int b =2,int c=3) {
+            Console.WriteLine(a + ", " + b + ", " + c);
+            }
+
+        private static string GetString(string prompt) {
+            Console.WriteLine(prompt);
+            string name = Console.ReadLine();
+            return name;
+            }
+
+        /*       private static int GetInt(string prompt) {
+                   int i = 0;
+                   bool success = false;
+
+                   while (!success) {
+                       Console.Write(prompt);
+                       string s = Console.ReadLine();
+                       try {
+                           i = Convert.ToInt32(s);
+                           success = true;
+                           }
+                       catch (FormatException) {
+                           Console.WriteLine("Error. Invalid entry!" + s);
+                           }
+
+                       }
+
+                   return i;
+                   }*/
+
+        private static int GetInt(string prompt) {
+            int i = 0;
+            bool success = false;
+
+            while (!success) {
+                Console.Write(prompt);
+                string s = Console.ReadLine();
+               if (int.TryParse(s, out i)) {
+                    success = true;
+                    }
+                else {
+                    Console.WriteLine("Error - not a whole number: " + s);
+                    }
+                }
+
+            return i;
+            }
+        }
+
+
+    }
